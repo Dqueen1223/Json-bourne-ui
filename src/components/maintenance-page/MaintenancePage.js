24d03837db125feb9468e52961a298ad59154ad8
Jay Conner <jconner@catalyte.io>
2022-01-12 15:14:02 -0600

    maintenance page displays a table without borders

--- src/components/maintenance-page/MaintenancePage.js
+++ src/components/maintenance-page/MaintenancePage.js
@@ -1,5 +1,29 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import fetchProducts from '../product-page/ProductPageService';
+import styles from '../../index.css';
+import Constants from '../../utils/constants';
 
-const MaintenancePage = () => (<div><p>Maintenance</p></div>);
+const MaintenancePage = () => {
+  const [products, setProducts] = useState([]);
+  const [apiError, setApiError] = useState(false);
 
+  useEffect(() => {
+    fetchProducts(setProducts, setApiError);
+  }, []);
+
+  return (
+    <div className="Maintenance">
+      {apiError && <p className={styles.errMsg} data-testid="errMsg">{Constants.API_ERROR}</p>}
+      {products.map((product) => (
+        <table className="ProductTable">
+          <tr key={product.id}>
+            {Object.values(product).map((val) => (
+              <td>{val}</td>
+            ))}
+          </tr>
+        </table>
+      ))}
+    </div>
+  );
+};
 export default MaintenancePage;
