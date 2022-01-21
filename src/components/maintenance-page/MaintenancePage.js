ae5fb98e0cd70a298ec0ade1dffd0fa19bfca93d
Jay Conner <jconner@catalyte.io>
2022-01-21 12:57:53 -0700

    adds button for T-22

--- src/components/maintenance-page/MaintenancePage.js
+++ src/components/maintenance-page/MaintenancePage.js
@@ -1,8 +1,10 @@
 import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 import fetchProducts from './MaintenancePageService';
 import './MaintenancePage.css';
 import styles from '../product-page/ProductPage.module.css';
 import Constants from '../../utils/constants';
+// import styles2 from '../checkout-page/CheckoutPage.module.css';
 
 /**
  * @name MaintenancePage
@@ -21,7 +23,15 @@ const MaintenancePage = () => {
     <div className="Maintenance">
       {apiError && <p className={styles.errMsg} data-testid="errMsg">{Constants.API_ERROR}</p>}
 
+      <div className="Buttons">
+        <Link to="/maintenance/create">
+          <button type="button" className="createButton">
+            Create Product
+          </button>
+        </Link>
+      </div>
       <div className="ProductTable">
+
         <table className="Product">
           <thead>
             <th>Id</th>
