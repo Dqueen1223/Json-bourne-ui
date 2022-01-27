eedf9018ee54f37c6fda1e75905951b49b4cd5f3
Jay Conner <jconner@catalyte.io>
2022-01-27 16:36:50 -0600

    adds brand filter checkboxes

--- src/components/product-page/filter-menu/FilterMenu.js
+++ src/components/product-page/filter-menu/FilterMenu.js
@@ -9,6 +9,14 @@ const FilterMenu = ({ setFilter, isActive }) => {
   const [menIsChecked, setMenIsChecked] = useState(false);
   const [womenIsChecked, setWomenIsChecked] = useState(false);
   const [kidsIsChecked, setKidsIsChecked] = useState(false);
+  const [nikeIsChecked, setNikeIsChecked] = useState(false);
+  const [reebokIsChecked, setReebokIsChecked] = useState(false);
+  const [asicsIsChecked, setAsicsIsChecked] = useState(false);
+  const [brooksIsChecked, setBrooksIsChecked] = useState(false);
+  const [skechersIsChecked, setSkechersIsChecked] = useState(false);
+  const [pumaIsChecked, setPumaIsChecked] = useState(false);
+  const [underArmorIsChecked, setUnderArmorIsChecked] = useState(false);
+  const [adidasIsChecked, setAdidasIsChecked] = useState(false);
 
   const checkboxReset = () => {
     setMenIsChecked(false);
@@ -46,41 +54,196 @@ const FilterMenu = ({ setFilter, isActive }) => {
     setKidsIsChecked(!kidsIsChecked);
   };
 
+  const handleNikeCheckbox = () => {
+    if (!nikeIsChecked) {
+      setFilter('&brand=Nike');
+    } else {
+      setFilter('');
+    }
+    setNikeIsChecked(!nikeIsChecked);
+  };
+
+  const handleReebokCheckbox = () => {
+    if (!reebokIsChecked) {
+      setFilter('&brand=Reebok');
+    } else {
+      setFilter('');
+    }
+    setReebokIsChecked(!reebokIsChecked);
+  };
+
+  const handleAsicsCheckbox = () => {
+    if (!nikeIsChecked) {
+      setFilter('&brand=Asics');
+    } else {
+      setFilter('');
+    }
+    setAsicsIsChecked(!asicsIsChecked);
+  };
+
+  const handleBrooksCheckbox = () => {
+    if (!asicsIsChecked) {
+      setFilter('&brand=Brooks');
+    } else {
+      setFilter('');
+    }
+    setBrooksIsChecked(!brooksIsChecked);
+  };
+
+  const handleSkechersCheckbox = () => {
+    if (!skechersIsChecked) {
+      setFilter('&brand=Skechers');
+    } else {
+      setFilter('');
+    }
+    setSkechersIsChecked(!skechersIsChecked);
+  };
+
+  const handlePumaCheckbox = () => {
+    if (!pumaIsChecked) {
+      setFilter('&brand=Puma');
+    } else {
+      setFilter('');
+    }
+    setPumaIsChecked(!pumaIsChecked);
+  };
+
+  const handleUnderArmorCheckbox = () => {
+    if (!underArmorIsChecked) {
+      setFilter('&brand=Under&#160;Armor');
+    } else {
+      setFilter('');
+    }
+    setUnderArmorIsChecked(!underArmorIsChecked);
+  };
+
+  const handleAdidasCheckbox = () => {
+    if (!adidasIsChecked) {
+      setFilter('&brand=Adidas');
+    } else {
+      setFilter('');
+    }
+    setAdidasIsChecked(!adidasIsChecked);
+  };
+
   return (
     <div className={isActive ? styles.sidebar : styles.sideCollapsed}>
       <div className={styles.filterCheckbox}>
-        <span className={styles.checkBoxLabel}>Products</span>
+        <span className={styles.checkBoxLabel}>Brand</span>
         <div className={styles.fieldset}>
-          <label htmlFor="mens">
+          <label htmlFor="nike">
             <input
-              id="mens"
+              id="nike"
               type="checkbox"
-              checked={menIsChecked}
-              onChange={handleMensCheckbox}
+              checked={nikeIsChecked}
+              onChange={handleNikeCheckbox}
             />
-            Mens
+            Nike
           </label>
           <br />
-          <label htmlFor="womens">
+          <label htmlFor="reebok">
             <input
-              id="womens"
+              id="reebok"
               type="checkbox"
-              checked={womenIsChecked}
-              onChange={handleWomensCheckbox}
+              checked={reebokIsChecked}
+              onChange={handleReebokCheckbox}
             />
-            Womens
+            Reebok
           </label>
           <br />
-          <label htmlFor="kids">
+          <label htmlFor="asics">
             <input
-              id="kids"
+              id="asics"
               type="checkbox"
-              checked={kidsIsChecked}
-              onChange={handleKidsCheckbox}
+              checked={asicsIsChecked}
+              onChange={handleAsicsCheckbox}
             />
-            Kids
+            Asics
           </label>
           <br />
+          <label htmlFor="brooks">
+            <input
+              id="brooks"
+              type="checkbox"
+              checked={brooksIsChecked}
+              onChange={handleBrooksCheckbox}
+            />
+            Brooks
+          </label>
+          <br />
+          <label htmlFor="skechers">
+            <input
+              id="skechers"
+              type="checkbox"
+              checked={skechersIsChecked}
+              onChange={handleSkechersCheckbox}
+            />
+            Skechers
+          </label>
+          <br />
+          <label htmlFor="puma">
+            <input
+              id="puma"
+              type="checkbox"
+              checked={pumaIsChecked}
+              onChange={handlePumaCheckbox}
+            />
+            Puma
+          </label>
+          <br />
+          <label htmlFor="underarmor">
+            <input
+              id="underarmor"
+              type="checkbox"
+              checked={underArmorIsChecked}
+              onChange={handleUnderArmorCheckbox}
+            />
+            Under Armor
+          </label>
+          <br />
+          <label htmlFor="adidas">
+            <input
+              id="adidas"
+              type="checkbox"
+              checked={adidasIsChecked}
+              onChange={handleAdidasCheckbox}
+            />
+            Adidas
+          </label>
+          <br />
+          <span className={styles.checkBoxLabel}>Demographics</span>
+          <div className={styles.fieldset}>
+            <label htmlFor="mens">
+              <input
+                id="mens"
+                type="checkbox"
+                checked={menIsChecked}
+                onChange={handleMensCheckbox}
+              />
+              Mens
+            </label>
+            <br />
+            <label htmlFor="womens">
+              <input
+                id="womens"
+                type="checkbox"
+                checked={womenIsChecked}
+                onChange={handleWomensCheckbox}
+              />
+              Womens
+            </label>
+            <br />
+            <label htmlFor="kids">
+              <input
+                id="kids"
+                type="checkbox"
+                checked={kidsIsChecked}
+                onChange={handleKidsCheckbox}
+              />
+              Kids
+            </label>
+            <br />
+          </div>
         </div>
       </div>
     </div>
