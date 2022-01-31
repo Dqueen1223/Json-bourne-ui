adcf47a411da121fe414c4476546cb4b131e1588
Jody Jones <jjones2@catalyte.io>
2022-01-30 21:20:34 -0500

    keep opening up other product card modal on button click. Modal also doesn't close if button has been clicked to bring it up vs if anywhere else on card has been clicked. Need to figure that out. An alert set up earlier worked fine but Modal is somehow being blocked.

--- src/components/product-page/ReviewsModal.js
+++ src/components/product-page/ReviewsModal.js
@@ -1,14 +1,42 @@
-/* eslint-disable no-alert */
-// import React, { useState } from 'react';
+import React, { useState } from 'react';
 import './ProductCardModal.css';
+import { Modal, Button } from 'react-bootstrap';
 
 /**
- * @name ProductCardReviewsModal
- * @description material-ui styling from review modal
- * @returns component
+ * @name ProductCardReviewModal
+ * @description material-ui styling for product card review modal
+ * @return component
  */
+
 const ProductCardReviewsModal = () => {
-  const myalert = alert('testing to make sure this works');
-  return myalert;
+  const [showModal, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  return (
+    <>
+      <div
+        className="d-flex align-items-center justify-content-center"
+        style={{ height: '100vh' }}
+      >
+        <Button variant="primary" onClick={handleShow}>
+          Launch demo modal
+        </Button>
+      </div>
+      <Modal show={showModal} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Review title</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>This should be the reviews area</Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
+  );
 };
+
 export default ProductCardReviewsModal;
