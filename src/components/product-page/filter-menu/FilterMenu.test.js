8956b9866bbe7b4e4353ce1b3c794dfe58b60ba7
Jay Conner <jconner@catalyte.io>
2022-02-07 23:21:10 -0600

    finishes unit tests

--- src/components/product-page/filter-menu/FilterMenu.test.js
+++ src/components/product-page/filter-menu/FilterMenu.test.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { cleanup, fireEvent, render } from '@testing-library/react';
-// import { screen } from '@testing-library/dom';
 import FilterMenu from './FilterMenu';
 import ProductPage from '../ProductPage';
 
@@ -24,110 +23,117 @@ describe('handleCheckbox', () => {
     }));
     expect(nikeCheckbox.checked).toEqual(true);
   });
-
-  // it('checks that handleCheckbox is returning the appropriate filter string', () => {
-  //   const { getByTestId } = render(<ProductPage><FilterMenu /></ProductPage>,
-  //     container);
-
-  //   const nikeCheckbox = getByTestId('nikeCheckbox');
-  //   fireEvent.click(nikeCheckbox, new MouseEvent('click', {
-  //     bubbles: true,
-  //     cancelable: true
-
-  //   }));
-  // });
-  // expect({filterArray}).toEqual(expect.arrayContaining('&Brand=Nike'));
-  describe('handlePrice', () => {
-    beforeEach(() => {
-      container = document.createElement('div');
-      document.body.appendChild(container);
-    });
-
-    afterEach(cleanup);
-
-    it('checks for a maximum price', () => {
-      render(<ProductPage><FilterMenu /></ProductPage>, container);
-      const minPriceInput = document.getElementById('minPrice');
-      const maxPriceInput = document.getElementById('maxPrice');
-      const priceErrors = document.getElementById('priceError');
-      const filterButton = document.getElementById('filterPrice');
-
-      minPriceInput.value = '12';
-      maxPriceInput.value = '';
-      fireEvent.click(filterButton);
-      expect(priceErrors.innerText).toEqual('Please input a maximum price.');
-    });
-
-    it('checks for a minimum price', () => {
-      render(<ProductPage><FilterMenu /></ProductPage>, container);
-      const minPriceInput = document.getElementById('minPrice');
-      const maxPriceInput = document.getElementById('maxPrice');
-      const priceErrors = document.getElementById('priceError');
-      const filterButton = document.getElementById('filterPrice');
-
-      minPriceInput.value = '';
-      maxPriceInput.value = '12';
-      fireEvent.click(filterButton);
-      expect(priceErrors.innerText).toEqual('Please input a minimum price.');
-    });
-
-    it('checks for both prices', () => {
-      render(<ProductPage><FilterMenu /></ProductPage>, container);
-      const minPriceInput = document.getElementById('minPrice');
-      const maxPriceInput = document.getElementById('maxPrice');
-      const priceErrors = document.getElementById('priceError');
-      const filterButton = document.getElementById('filterPrice');
-
-      minPriceInput.value = '';
-      maxPriceInput.value = '';
-      fireEvent.click(filterButton);
-      expect(priceErrors.innerText).toEqual('Please input prices to sort by.');
-    });
-
-    it('checks for minimum price less than maximum price', () => {
-      render(<ProductPage><FilterMenu /></ProductPage>, container);
-      const minPriceInput = document.getElementById('minPrice');
-      const maxPriceInput = document.getElementById('maxPrice');
-      const priceErrors = document.getElementById('priceError');
-      const filterButton = document.getElementById('filterPrice');
-
-      minPriceInput.value = '15';
-      maxPriceInput.value = '12';
-      fireEvent.click(filterButton);
-      expect(priceErrors.innerText).toEqual('Minimum price must be less than the maximum price.');
-    });
-
-    it('checks for minimum price less than maximum price', () => {
-      render(<ProductPage><FilterMenu /></ProductPage>, container);
-      const minPriceInput = document.getElementById('minPrice');
-      const maxPriceInput = document.getElementById('maxPrice');
-      const priceErrors = document.getElementById('priceError');
-      const filterButton = document.getElementById('filterPrice');
-
-      minPriceInput.value = '15';
-      maxPriceInput.value = '12';
-      fireEvent.click(filterButton);
-      expect(priceErrors.innerText).toEqual('Minimum price must be less than the maximum price.');
-    });
+  it('unchecks the checkboxes', () => {
+    const { getByTestId } = render(<ProductPage><FilterMenu /></ProductPage>,
+      container);
+    const nikeCheckbox = getByTestId('nikeCheckbox');
+    expect(nikeCheckbox.checked).toEqual(false);
+    fireEvent.click(nikeCheckbox, new MouseEvent('click', {
+      bubbles: true,
+      cancelable: true
+    }));
+    fireEvent.click(nikeCheckbox, new MouseEvent('click', {
+      bubbles: true,
+      cancelable: true
+    }));
+    expect(nikeCheckbox.checked).toEqual(false);
+  });
+});
+describe('handlePrice', () => {
+  beforeEach(() => {
+    container = document.createElement('div');
+    document.body.appendChild(container);
   });
 
-  describe('removePrice', () => {
-    it('removes price values', () => {
-      render(<ProductPage><FilterMenu /></ProductPage>, container);
-      const minPriceInput = document.getElementById('minPrice');
-      const maxPriceInput = document.getElementById('maxPrice');
-      const clearButton = document.getElementById('clearPrice');
-      const minPriceResult = document.getElementById('minPrice').value;
-      const maxPriceResult = document.getElementById('maxPrice').value;
+  afterEach(cleanup);
 
-      minPriceInput.value = '12';
-      maxPriceInput.value = '18';
-      fireEvent.click(clearButton, new MouseEvent('click', {
-        bubbles: true,
-        cancelable: true
-      }));
-      expect(minPriceResult).toEqual('');
-      expect(maxPriceResult).toEqual('');
-    });
+  it('checks for a maximum price', () => {
+    render(<ProductPage><FilterMenu /></ProductPage>, container);
+    const minPriceInput = document.getElementById('minPrice');
+    const maxPriceInput = document.getElementById('maxPrice');
+    const priceErrors = document.getElementById('priceError');
+    const filterButton = document.getElementById('filterPrice');
+
+    minPriceInput.value = '12';
+    maxPriceInput.value = '';
+    fireEvent.click(filterButton);
+    expect(priceErrors.innerText).toEqual('Please input a maximum price.');
+  });
+
+  it('checks for a minimum price', () => {
+    render(<ProductPage><FilterMenu /></ProductPage>, container);
+    const minPriceInput = document.getElementById('minPrice');
+    const maxPriceInput = document.getElementById('maxPrice');
+    const priceErrors = document.getElementById('priceError');
+    const filterButton = document.getElementById('filterPrice');
+
+    minPriceInput.value = '';
+    maxPriceInput.value = '12';
+    fireEvent.click(filterButton);
+    expect(priceErrors.innerText).toEqual('Please input a minimum price.');
+  });
+
+  it('checks for both prices', () => {
+    render(<ProductPage><FilterMenu /></ProductPage>, container);
+    const minPriceInput = document.getElementById('minPrice');
+    const maxPriceInput = document.getElementById('maxPrice');
+    const priceErrors = document.getElementById('priceError');
+    const filterButton = document.getElementById('filterPrice');
+
+    minPriceInput.value = '';
+    maxPriceInput.value = '';
+    fireEvent.click(filterButton);
+    expect(priceErrors.innerText).toEqual('Please input prices to sort by.');
+  });
+
+  it('checks for minimum price less than maximum price', () => {
+    render(<ProductPage><FilterMenu /></ProductPage>, container);
+    const minPriceInput = document.getElementById('minPrice');
+    const maxPriceInput = document.getElementById('maxPrice');
+    const priceErrors = document.getElementById('priceError');
+    const filterButton = document.getElementById('filterPrice');
+
+    minPriceInput.value = '15';
+    maxPriceInput.value = '12';
+    fireEvent.click(filterButton);
+    expect(priceErrors.innerText).toEqual('Minimum price must be less than the maximum price.');
+  });
+
+  it('checks for minimum price less than maximum price', () => {
+    render(<ProductPage><FilterMenu /></ProductPage>, container);
+    const minPriceInput = document.getElementById('minPrice');
+    const maxPriceInput = document.getElementById('maxPrice');
+    const priceErrors = document.getElementById('priceError');
+    const filterButton = document.getElementById('filterPrice');
+
+    minPriceInput.value = '15';
+    maxPriceInput.value = '12';
+    fireEvent.click(filterButton);
+    expect(priceErrors.innerText).toEqual('Minimum price must be less than the maximum price.');
+  });
+});
+
+describe('removePrice', () => {
+  it('removes price values', () => {
+    render(<ProductPage><FilterMenu /></ProductPage>, container);
+    const minPriceInput = document.getElementById('minPrice');
+    const maxPriceInput = document.getElementById('maxPrice');
+    const filterButton = document.getElementById('filterPrice');
+    const clearButton = document.getElementById('clearPrice');
+    const minPriceResult = document.getElementById('minPrice').value;
+    const maxPriceResult = document.getElementById('maxPrice').value;
+
+    minPriceInput.value = '12';
+    maxPriceInput.value = '18';
+    fireEvent.click(filterButton, new MouseEvent('click', {
+      bubbles: true,
+      cancelable: true
+    }));
+    fireEvent.click(clearButton, new MouseEvent('click', {
+      bubbles: true,
+      cancelable: true
+    }));
+    expect(minPriceResult).toEqual('');
+    expect(maxPriceResult).toEqual('');
   });
 });
