73e2eb445d2682194aaf73162c2a82d96d1a28ba
Slate Lucia <slucia@catalyte.io>
2022-02-23 12:33:26 -0500

    fixed validation bug

--- src/components/Profile/Validation.js
+++ src/components/Profile/Validation.js
@@ -1,13 +1,18 @@
 /* eslint-disable no-param-reassign */
 const profileValidation = (data) => {
   const validateRequiredField = (object, field, errors) => {
-    if (object[field] === null || object[field].trim() === '') {
+    if (object[field] === null || object[field] === undefined) {
+      errors[field] = 'This field is required';
+    } else if (object[field].trim() === '') {
       errors[field] = 'This field is required';
     }
   };
 
   const validateAllFields = (information) => {
     const errors = {};
+    if (data.state === 'Select a state') {
+      errors.state = 'This field is required';
+    }
     validateRequiredField(information, 'firstName', errors);
     validateRequiredField(information, 'lastName', errors);
     validateRequiredField(information, 'street', errors);
